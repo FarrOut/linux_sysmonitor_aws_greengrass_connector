146c777ac7a73bb7b1f62f653e161246109f8a20
Greg Farr <farr.gs@gmail.com>
2020-08-17 13:57:53 +0200

    function to print cpu temp to screen

---
+++ temp_monitor.py
@@ -0,0 +1,10 @@
+# import greengrasssdk
+import subprocess
+
+# client = greengrasssdk.client('iot-data')
+
+# publish temperature metric through MQTT topic
+def main(thermal_zone):
+    filename = '/sys/class/thermal/thermal_zone'+ str(thermal_zone) + '/temp'
+    temp = subprocess.Popen(['cat', filename], stdout = subprocess.PIPE)
+    print (str(temp.communicate()))
