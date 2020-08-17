de54c57a27cde8220e0a625c049ee1fa914c637d
Greg Farr <farr.gs@gmail.com>
2020-08-17 14:50:08 +0200

    publish to MQTT topic

--- temp_monitor.py
+++ temp_monitor.py
@@ -1,10 +1,11 @@
-# import greengrasssdk
+import greengrasssdk
 import subprocess
 
-# client = greengrasssdk.client('iot-data')
+client = greengrasssdk.client('iot-data')
 
 # publish temperature metric through MQTT topic
-def main(thermal_zone = 0):
+def publish(thermal_zone = 0):
     filename = '/sys/class/thermal/thermal_zone'+ str(thermal_zone) + '/temp'
-    temp = subprocess.Popen(['cat', filename], stdout = subprocess.PIPE)
-    print (str(temp.communicate()))
+    temp = subprocess.check_output(['cat', filename])
+    client.publish(topic='metric/temp', payload=temp)    
+    print (temp)
