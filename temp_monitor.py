e54c0cbfbf5809b545bd0073d2dae00b5a74a2a0
Greg Farr <farr.gs@gmail.com>
2020-08-17 14:02:08 +0200

    overloaded function param with default value

--- temp_monitor.py
+++ temp_monitor.py
@@ -4,7 +4,7 @@ import subprocess
 # client = greengrasssdk.client('iot-data')
 
 # publish temperature metric through MQTT topic
-def main(thermal_zone):
+def main(thermal_zone = 0):
     filename = '/sys/class/thermal/thermal_zone'+ str(thermal_zone) + '/temp'
     temp = subprocess.Popen(['cat', filename], stdout = subprocess.PIPE)
     print (str(temp.communicate()))
